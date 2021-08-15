5ecd5ebbb5536daf8159548baf0a65767b3f89e0
mrDurandall <mrdurandall@gmail.com>
2021-08-15 13:08:16 +0300

    Выполнено задание 6*: Добавлена загрузка данных о продуктах из внешнего json файла

--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+import json
+
 
 def index(request):
     context = {
@@ -10,38 +12,12 @@ def index(request):
 
 
 def products(request):
+    with open('products/fixtures/products.json', encoding='utf-8') as json_prod:
+        products = json.load(json_prod)
+
     context = {
-        'products': [
-            {'name': 'Худи черного цвета с монограммами adidas Originals',
-             'price': 6_090.00,
-             'image': 'Adidas-hoodie.png',
-             'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.'},
+        'products': products,
 
-            {'name': 'Синяя куртка The North Face',
-             'price': 23_725.00,
-             'image': 'Blue-jacket-The-North-Face.png',
-             'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.'},
-
-            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN',
-             'price': 3_390.00,
-             'image': 'Brown-sports-oversized-top-ASOS-DESIGN.png',
-             'description': 'Материал с плюшевой текстурой. Удобный и мягкий.'},
-
-            {'name': 'Черный рюкзак Nike Heritage',
-             'price': 2_340.00,
-             'image': 'Black-Nike-Heritage-backpack.png',
-             'description': 'Плотная ткань. Легкий материал.'},
-
-            {'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex',
-             'price': 13_590.00,
-             'image': 'Black-Dr-Martens-shoes.png',
-             'description': 'Гладкий кожаный верх. Натуральный материал.'},
-
-            {'name': 'Темно-синие широкие строгие брюки ASOS DESIGN',
-             'price': 2_890.00,
-             'image': 'Dark-blue-wide-leg-ASOs-DESIGN-trousers.png',
-             'description': 'Легкая эластичная ткань сирсакер Фактурная ткань.'},
-        ],
         'title': 'GeekShop - Каталог',
     }
     return render(request, 'products/products.html', context)
