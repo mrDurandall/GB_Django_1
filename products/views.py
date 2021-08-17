e3c5a4aeefad64eb669d8a0a2f50547512757036
mrDurandall <mrdurandall@gmail.com>
2021-08-17 20:43:45 +0300

    Выполнено задание 4: Шаблоны и контроллеры переписаны для работы с моделями. Перед этим выполнены задания 2 и 3. Категории товаров добавлены через консоль, товары добавлены через админку. Возник вопрос по поводу того, как ссылаться на медиа файлы в шаблоне. Мой вариант работает, но может есть что-то еще? Оставил в том месте коммент. Надеюсь подскажете.

--- products/views.py
+++ products/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 import json
 
+from .models import Product, ProductCategory
+
 
 def index(request):
     context = {
@@ -12,11 +14,15 @@ def index(request):
 
 
 def products(request):
-    with open('products/fixtures/products.json', encoding='utf-8') as json_prod:
-        products = json.load(json_prod)
+    # with open('products/fixtures/products.json', encoding='utf-8') as json_prod:
+    #     products = json.load(json_prod)
+
+    products = Product.objects.all()
+    categories = ProductCategory.objects.all()
 
     context = {
         'products': products,
+        'categories': categories,
 
         'title': 'GeekShop - Каталог',
     }
