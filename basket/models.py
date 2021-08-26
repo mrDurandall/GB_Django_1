6792c36b22e2a1013f0d01884966ee190b934dad
mrDurandall <mrdurandall@gmail.com>
2021-08-26 22:30:04 +0300

    Подсчет полной стоимости корзины и количества товаров в коризне вынесены в методы модели корзины

--- basket/models.py
+++ basket/models.py
@@ -16,3 +16,11 @@ class Basket(models.Model):
 
     def sum(self):
         return self.product.price * self.quantity
+
+    def total_quantity(self):
+        baskets = Basket.objects.filter(user=self.user)
+        return sum(basket.quantity for basket in baskets)
+
+    def total_price(self):
+        baskets = Basket.objects.filter(user=self.user)
+        return sum(basket.sum() for basket in baskets)
--- user/views.py
+++ user/views.py
@@ -53,19 +53,11 @@ def profile(request):
         form = UserProfileForm(instance=request.user)
 
     baskets = Basket.objects.filter(user=request.user)
-    total_baskets_quantity = 0
-    total_baskets_price = 0
-
-    for basket in baskets:
-        total_baskets_quantity += basket.quantity
-        total_baskets_price += basket.sum()
 
     context = {
         'title': 'GeekShop - Профиль',
         'form': form,
         'baskets': baskets,
-        'total_quantity': total_baskets_quantity,
-        'total_price': total_baskets_price,
     }
 
     return render(request, 'user/profile.html', context)
