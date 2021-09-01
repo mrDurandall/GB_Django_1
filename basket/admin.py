4bab26200be3b749593f52f46af01bb196438e11
mrDurandall <mrdurandall@gmail.com>
2021-09-01 20:15:17 +0300

    Выполнено задание 5*: Изменена админка Джанго (полностью аналогично тому, что было сделано на уроке)

--- basket/admin.py
+++ basket/admin.py
@@ -4,5 +4,9 @@ from django.contrib import admin
 from basket.models import Basket
 
 
-admin.site.register(Basket)
+class BasketAdmin(admin.TabularInline):
+    model = Basket
+    fields = ('product', 'quantity', 'created_timestamp')
+    readonly_fields = ('created_timestamp', )
+    extra = 0
 
--- products/admin.py
+++ products/admin.py
@@ -6,5 +6,14 @@ from products.models import ProductCategory, Product
 
 
 admin.site.register(ProductCategory)
-admin.site.register(Product)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity')
+    fields = ('name', 'image', 'description', ('price', 'quantity'), 'category')
+    readonly_fields = ('description', )
+    ordering = ('-name', )
+    search_fields = ('name', )
+
 
