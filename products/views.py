1ac91b4beb9b08569087b5d89d556f91a53dd2fe
mrDurandall <mrdurandall@gmail.com>
2021-09-05 21:03:33 +0300

    Выполнено задание 2: Встроен пагинатор для отображения постраничного списка товаров

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from .models import Product, ProductCategory
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def index(request):
@@ -10,14 +11,22 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 
-def products(request, category_id=None):
+def products(request, category_id=None, page=1):
     if category_id:
         products = Product.objects.filter(category_id=category_id)
     else:
         products = Product.objects.all()
+
+    paginator = Paginator(products, per_page=3)
+    try:
+        products_paginator = paginator.page(page)
+    except PageNotAnInteger:
+        products_paginator = paginator.page(1)
+    except EmptyPage:
+        products_paginator = paginator.page(paginator.num_pages)
     categories = ProductCategory.objects.all()
     context = {
-        'products': products,
+        'products': products_paginator,
         'categories': categories,
         'title': 'GeekShop - Каталог',
     }
