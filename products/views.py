6c95dbb469c48b5e14b6458e20bde68f6a7988e8
mrDurandall <mrdurandall@gmail.com>
2021-09-05 20:18:18 +0300

    Выполнено задание 1: Организована фильтрация товаров в каталоге по категориям

--- products/views.py
+++ products/views.py
@@ -10,15 +10,15 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 
-def products(request):
-
-    products = Product.objects.all()
+def products(request, category_id=None):
+    if category_id:
+        products = Product.objects.filter(category_id=category_id)
+    else:
+        products = Product.objects.all()
     categories = ProductCategory.objects.all()
-
     context = {
         'products': products,
         'categories': categories,
-
         'title': 'GeekShop - Каталог',
     }
     return render(request, 'products/products.html', context)
