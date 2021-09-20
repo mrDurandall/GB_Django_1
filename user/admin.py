5234990aa4a8525619e6f20fb8eb689ffb1ac61a
mrDurandall <mrdurandall@gmail.com>
2021-09-20 19:58:48 +0300

    Добавлена связанная таблица профиля пользователя. Добавлена форма ее редактирования (пока без украшений)

--- user/admin.py
+++ user/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
-from user.models import User
+from user.models import User, UserProfile
 from basket.admin import BasketAdmin
+
+
+admin.site.register(UserProfile)
 
 
 @admin.register(User)
