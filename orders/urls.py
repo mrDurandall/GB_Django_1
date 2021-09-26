be42b7e67e5fb77c8e6f3d037ab6cc4729119ec2
mrDurandall <mrdurandall@gmail.com>
2021-09-26 23:36:51 +0300

    Созданы вьюхи для работы с заказом. Сделаны шаблоны и страницы для обработки заказа. Пока некрасивые. Потом поработаю с бутстрапом и сделаю всё красиво.

--- orders/urls.py
+++ orders/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
 
-# from orders.views import
+from orders.views import OrderListView,\
+    OrderCreateView,\
+    OrderUpdateView, OrderDeleteView, order_complete
 
 
 app_name = 'orders'
 
 urlpatterns = [
-    # path('', ProductsListView.as_view(), name='product'),
+    path('/', OrderListView.as_view(), name='index'),
+    path('new/', OrderCreateView.as_view(), name='new'),
+    path('update/<int:pk>/', OrderUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>/', OrderDeleteView.as_view(), name='delete'),
+    path('complete/<int:pk>/', order_complete, name='complete'),
 
 ]
+
